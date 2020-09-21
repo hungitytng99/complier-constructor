fae3e0d0d9fd822fc0fe7f0fb2e93e318522cd4d
Hung <mhungict99@gmail.com>
2020-09-21 21:11:35 +0700

    complete scanner

---
+++ Scanner(m)/incompleted/test.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int a = 1; 
+    switch (a)
+    {
+    case 1:
+        printf("GO");
+        break;
+    default:
+        printf("GO@");
+    }
+}
